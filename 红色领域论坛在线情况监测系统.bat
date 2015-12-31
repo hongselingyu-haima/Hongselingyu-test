5676d6d12455806524a30bf586f947b2b416ad97
hongselingyu-haima <postmaster@hongselingyu.xyz>
2015-12-31 22:52:12 +0800

    Update 红色领域论坛在线情况监测系统.bat

--- 红色领域论坛在线情况监测系统.bat
+++ 红色领域论坛在线情况监测系统.bat
@@ -1,4 +1,33 @@
-ping www.hongselingyu.xyz
+@echo off
+echo *********************欢迎使用红色领域网络检测工具【此为第一版(Beta版）】**************************
+echo *********************红色领域网络技术论坛：http://www.hkjs.xyz
+echo 正在检测能否访问百度
+@echo off
+ping www.baidu.com
+echo 检测完成
+echo 正在检测能否访问360官网
+@echo off
+ping www.360.com
+echo 检测完成
+@echo off
+echo 正在检测能否访问新浪
+ping www.sina.com
+echo 检测完成
+@echo off
+echo 正在检测能否访问网易
+ping www.163.com
+echo 检测完成
+@echo off
+echo 正在检测能否访问Twitter
+ping twitter.com
+echo 检测完成
+@echo off
+echo *********************已全部检测完成*********************
+echo 在显示下一行信息后按任意键将退出程序
 pause
-echo 监测完成，即将退出
+echo 即将退出..........................
+pause
+echo 感谢您的使用！ By红色领域编程组 于2015年12月
+echo 即将退出
+pause
 exit
